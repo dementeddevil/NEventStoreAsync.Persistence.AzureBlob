c17f80c151bf0d9572face6fdad92cc99ea2f5e2
Adrian Lewis <zen_global@hotmail.com>
2019-01-14 20:47:16 +0000

    Renamed project assembly file to match project style

---
+++ src/NEventStore.Persistence.AzureBlob/Properties/ProjectAssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NEventStoreAsync.Persistence.AzureBlob")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NEventStoreAsync.Persistence.AzureBlob")]
+[assembly: AssemblyCopyright("Copyright © NEventStore Dev Team 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("21693979-bf65-4bc1-abea-dedda3aa4a9d")]
+
+[assembly: AssemblyInformationalVersion("1.0.0-alpha.1+Branch.master.Sha.721bd4fcddc16f4ed6cb9bdd9577bb30f141f8ce")]
